0b61ce665bcc100c38f10e7733b6db6541d0d889
jun zhang <zhangjun20071455@gmail.com>
2020-07-20 20:54:28 -0700

    Updates

---
+++ precache-manifest.b8adfe69ac4bd611846ccfc38eeff389.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b069eb863b542d658aaf0286586d4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "121789d392a056ebe73d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ad4e726366ac3783b35c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "121789d392a056ebe73d",
+    "url": "/static/js/2.a34dd6c8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a34dd6c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad4e726366ac3783b35c",
+    "url": "/static/js/main.a69cd9ed.chunk.js"
+  },
+  {
+    "revision": "fc691b42df1c301092b6",
+    "url": "/static/js/runtime-main.0c8c1af8.js"
+  }
+]);
